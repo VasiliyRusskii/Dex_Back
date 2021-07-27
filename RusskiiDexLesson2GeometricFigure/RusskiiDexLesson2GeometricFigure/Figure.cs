86167f621ab0c47387321b9fba5bbd480644e8dc
N5000 <vsk456654@gmail.com>
2021-07-27 23:38:21 +0300

    Add class Figure

---
+++ RusskiiDexLesson2GeometricFigure/RusskiiDexLesson2GeometricFigure/Figure.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace RusskiiDexLesson2GeometricFigure
+{
+    public class Figure
+    {
+        public int countOfEdge { get; set; }
+        public double lenghtOfEdge { get; set; }
+        public double square { get; set; }
+
+        public Figure(int countOfEdge, double lenghtOfEdge)
+        {
+            this.countOfEdge = countOfEdge;
+            this.lenghtOfEdge = lenghtOfEdge;
+        }
+
+        public Figure(int countOfEdge, double lenghtOfEdge, double square)
+        {
+            this.countOfEdge = countOfEdge;
+            this.lenghtOfEdge = lenghtOfEdge;
+            this.square = square;
+        }
+    }
+}
---
+++ RusskiiDexLesson2GeometricFigure/RusskiiDexLesson2GeometricFigure/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+
+namespace RusskiiDexLesson2GeometricFigure
+{
+    class Program
+    {
+        //Рассмотрим на примере куба 
+        public static int countOfSquares = 6; 
+        public static int countOfEdges = 12;
+        public static double lenghtOfEdge = 8;
+
+        static void Main(string[] args)
+        {
+            Figure figure = new Figure(countOfEdges, lenghtOfEdge);
+            Console.WriteLine("Исходное значение количество граней: " + figure.countOfEdge
+                + " Длина граней: " + figure.lenghtOfEdge);
+
+            figure = counting(figure);
+            Console.WriteLine("Полученное значение количество граней: " + figure.countOfEdge
+                + " Длина граней: " + figure.lenghtOfEdge + "\nПлощадь фигуры: " + figure.square);
+        }
+
+        static Figure counting(Figure figure)
+        {
+            return new Figure(figure.countOfEdge, figure.lenghtOfEdge, Math.Pow(figure.lenghtOfEdge, 2) * countOfSquares);
+        }
+
+
+    }
+}
