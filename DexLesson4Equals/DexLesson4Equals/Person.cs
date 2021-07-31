76c3519319ac6d8bb9968e3202100e7df2035393
N5000 <vsk456654@gmail.com>
2021-07-31 23:31:17 +0300

    DexLesson4Equals

---
+++ DexLesson4Equals/DexLesson4Equals/Person.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace DexLesson4Equals
+{
+    public class Person
+    {
+        public string lastFirstMiddleName { get; set; }
+        public string birthDay { get; set; }
+        public string placeOfBirth { get; set; }
+        public string numberPassword { get; set; }
+
+        public Person(string lastFirstMiddleNamePerson, string birthDayPerson, string placeOfBirthPerson, string numberPasswordPerson)
+        {
+            lastFirstMiddleName = lastFirstMiddleNamePerson;
+            birthDay = birthDayPerson;
+            placeOfBirth = placeOfBirthPerson;
+            numberPassword = numberPasswordPerson;
+        }
+
+
+        public override bool Equals(object obj)
+        {
+            if (obj == null)
+                return false;
+            Person person = obj as Person;
+            if (person as Person == null)
+                return false;
+            return person.lastFirstMiddleName == this.lastFirstMiddleName && person.birthDay == this.birthDay 
+                && person.placeOfBirth == this.placeOfBirth && numberPassword == this.numberPassword;
+        }
+        public bool Equals(Person obj) 
+        {
+            if (obj == null)
+                return false;
+            return obj.lastFirstMiddleName == this.lastFirstMiddleName && obj.birthDay == this.birthDay &&
+                obj.placeOfBirth == this.placeOfBirth && obj.numberPassword == this.numberPassword;
+        }
+        public override int GetHashCode()
+        {
+            int lastFirstMiddleNameGet;
+
+            if (lastFirstMiddleName == "Ivan Olegovich Naumov")
+            {
+                return lastFirstMiddleNameGet = 101;
+            }
+            else
+            {
+                return lastFirstMiddleNameGet = 102;
+            }
+            
+        }
+
+    }
+}
---
+++ DexLesson4Equals/DexLesson4Equals/Program.cs
@@ -0,0 +1,39 @@
+﻿using System;
+
+namespace DexLesson4Equals
+{
+    public class Money
+    {
+        public decimal Amount { get; set; }
+        public string Unit { get; set; }
+
+        public Money(decimal amount, string unit)
+        {
+            Amount = amount;
+            Unit = unit;
+        }
+
+
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Рассмотрим оператор равенства "=="
+            string namePersonCompany = "Tesla";
+            string namePersonSchool = "Tiraspol";
+            string variousName = namePersonCompany;
+            Console.WriteLine(namePersonCompany == namePersonSchool); // false
+            Console.WriteLine(namePersonCompany == variousName); // true
+
+            //Equals и GetHashCode
+            Person personOne = new Person("Ivan Olegovich Naumov", "03.06.2021", "PMR", "IP-40003" );
+            Person personTwo = new Person("Ivan Olegovich Naumov", "03.06.2021", "PMR", "IP-40003");
+            Person personThree = new Person("Oleg Olegovich Naumov", "03.09.2021", "Russian", "IP-40004");
+            Console.WriteLine(personOne.GetHashCode()); // 101
+            Console.WriteLine(personTwo.GetHashCode()); // 101
+            Console.WriteLine(personThree.GetHashCode()); // 102
+            Console.ReadLine();
+        }
+    }
+}
